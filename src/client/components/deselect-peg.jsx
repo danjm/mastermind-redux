f1fb1ee60ca6ebd40d78a5cfb14bf3e3d3576b80
Dan Miller <danmiller@bluedrop.com>
2016-12-11 21:06:53 -0330

    Adds deselect peg.

---
+++ src/client/components/deselect-peg.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import FaTimesCircle from 'react-icons/fa/times-circle';
+import SelectPeg from '../containers/select-peg';
+
+const DeselectPeg = () => (<SelectPeg num={-1} >
+	<FaTimesCircle size={40} style={{ margin: '-5px -9px' }} />
+</SelectPeg>);
+
+export default DeselectPeg;
--- src/client/components/select-column.jsx
+++ src/client/components/select-column.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { map, range } from 'lodash';
 import SelectPeg from '../containers/select-peg';
+import DeselectPeg from '../components/deselect-peg';
 
 const selectColumnStyle = {
 	width: '45px',
@@ -11,7 +12,8 @@ const selectColumnStyle = {
 const pegs = map(range(0, 6), num => <SelectPeg key={`'peg'${num}`} num={num} />);
 
 const SelectColumn = () => (<div style={selectColumnStyle}>
-	{pegs}
+	{pegs},
+	<DeselectPeg />
 </div>);
 
 export default SelectColumn;
