f1996bd9e04d0c63a4073fbe70e41d5dfc1d05ee
Dan Miller <danmiller@bluedrop.com>
2016-12-11 21:00:05 -0330

    Active row is highlighted.

--- src/client/components/peg-row.jsx
+++ src/client/components/peg-row.jsx
@@ -2,18 +2,19 @@ import React, { PropTypes } from 'react';
 import GamePeg from '../containers/game-peg';
 import Checkmark from '../containers/checkmark';
 
-const regRowStyle = {
-	width: '97%',
+const pegRowStyle = isActive => ({
+	width: isActive ? '96%' : '97%',
 	height: '7.4%',
 	backgroundColor: '#75cbe1',
 	marginLeft: '1%',
 	marginRight: '1%',
 	marginTop: '1%',
 	paddingTop: '2px',
-	paddingLeft: '2px'
-};
+	paddingLeft: '2px',
+	border: isActive ? '2px solid #FF9800' : '0px'
+});
 
-const PegRow = ({ rowNum }) => (<div style={regRowStyle} >
+const PegRow = ({ activeRow, rowNum }) => (<div style={pegRowStyle(activeRow === rowNum)} >
 	<GamePeg num={0} row={rowNum} />
 	<GamePeg num={1} row={rowNum} />
 	<GamePeg num={2} row={rowNum} />
@@ -22,6 +23,7 @@ const PegRow = ({ rowNum }) => (<div style={regRowStyle} >
 </div>);
 
 PegRow.propTypes = {
+	activeRow: PropTypes.number.isRequired,
 	rowNum: PropTypes.number.isRequired
 };
 
--- src/client/containers/peg-row.js
+++ src/client/containers/peg-row.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import PegRow from '../components/peg-row';
 
 const mapStateToProps = (state, ownProps) => ({
+	activeRow: state.game.activeRow,
 	rowNum: ownProps.rowNum
 });
 
