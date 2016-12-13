6b40f26c478875bcbe422137eef3f09ba73091fe
Dan Miller <danmiller@bluedrop.com>
2016-12-12 23:43:56 -0330

    Optional border for pegs

--- src/client/components/deselect-peg.jsx
+++ src/client/components/deselect-peg.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FaTimesCircle from 'react-icons/fa/times-circle';
 import SelectPeg from '../containers/select-peg';
 
-const DeselectPeg = () => (<SelectPeg num={null} >
+const DeselectPeg = () => (<SelectPeg num={null} borderSize={0}>
 	<FaTimesCircle size={40} style={{ margin: '-5px -9px' }} />
 </SelectPeg>);
 
--- src/client/components/peg.jsx
+++ src/client/components/peg.jsx
@@ -1,21 +1,21 @@
 import React, { PropTypes } from 'react';
 import { get } from 'lodash';
 
-const colours = ['#e982ff', '#FFC107', '#00BCD4', '#5ee463', '#FFEB3B', '#ff584b'];
+const colours = ['#e982ff', '#FF9800', '#00BCD4', '#5ee463', '#FFEB3B', '#ff584b'];
 
-const pegStyle = pegNumber => ({
+const pegStyle = (pegNumber, borderSize = 1) => ({
 	width: '36px',
 	height: '36px',
 	borderRadius: '50%',
 	background: get(colours, pegNumber, 'white'),
 	marginLeft: '2%',
-	border: `${pegNumber === null ? 0 : 1}px solid black`,
+	border: `${borderSize}px solid black`,
 	float: 'left'
 });
 
-const Peg = ({ selectPeg, selectedPeg, colourNum, children }) =>
+const Peg = ({ selectPeg, selectedPeg, colourNum, children, borderSize }) =>
 	<button
-		style={pegStyle(colourNum)}
+		style={pegStyle(colourNum, borderSize)}
 		onClick={() => { selectPeg(selectedPeg); }}
 	>{children}
 	</button>;
@@ -24,7 +24,8 @@ Peg.propTypes = {
 	selectPeg: PropTypes.func.isRequired,
 	selectedPeg: PropTypes.number,
 	children: PropTypes.element,
-	colourNum: PropTypes.number
+	colourNum: PropTypes.number,
+	borderSize: PropTypes.number
 };
 
 export default Peg;
